1141a677cae292f1f22e8392a7aaf58891146e86
Andi.Stegmann <Andi.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-05-14 19:53:21 +0000

    Speicherladepumpen auch einschalten, wenn KES_Tvl_MW > Speichertemp.

--- HeizungRCA_V0.4/speicher.c
+++ HeizungRCA_V0.4/speicher.c
@@ -24,7 +24,7 @@ void cntrl_Speicher( void )
      ********************************************/
     if( SOL_SP1_To_MW < sol_SP1_To_SW_f ) {
         kes_Tvl_SW_Sp1_f = sol_SP1_To_SW_f + kes_sp_dt_sw;
-        if( KES_BR_BM == IO_EIN ) KES_PU_SP1_SB = IO_EIN;
+        if( (KES_BR_BM == IO_EIN) || (KES_Tvl_MW > SOL_SP1_To_MW) ) KES_PU_SP1_SB = IO_EIN;
         /* Wenn Sp.-pumpe 1 ein, Sp.-pumpe 2 immer aus! */
         KES_PU_SP2_SB = IO_AUS;
     }
@@ -41,7 +41,7 @@ void cntrl_Speicher( void )
         /* Sp.-pumpe 2 nur einschalten, wenn Sp.-pumpe 1 aus ist! */
         if( SOL_SP2_To_MW < sol_SP2_To_SW_f ) {
             kes_Tvl_SW_Sp2_f = sol_SP2_To_SW_f + kes_sp_dt_sw;
-            if( KES_BR_BM == IO_EIN ) KES_PU_SP2_SB = IO_EIN;
+            if( (KES_BR_BM == IO_EIN) || (KES_Tvl_MW > SOL_SP2_To_MW) ) KES_PU_SP2_SB = IO_EIN;
         }
         else if( SOL_SP2_To_MW >= (sol_SP2_To_SW_f + kes_sp_dt_sw/2.0) ) {
             kes_Tvl_SW_Sp2_f = 0.0;  /* Kessel AUS */
