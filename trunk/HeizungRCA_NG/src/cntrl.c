282074d19cff0020bb4ebb6b84f3bc653bee506b
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-09-10 09:55:23 +0000

    globale Variablen über alle Threads sichtbar

--- trunk/HeizungRCA_NG/src/cntrl.c
+++ trunk/HeizungRCA_NG/src/cntrl.c
@@ -1,3 +1,4 @@
+#define _CNTRL_C_
 #define __IO_MASTER__
 
 #include <stdio.h>
@@ -12,7 +13,8 @@
 #include "hk.h"
 #include "ww.h"
 #include "kes.h"
-// #include "io.h"
+#include "io.h"
+#include "cntrl.h"
 
 #ifdef __WIN__
 #include <windows.h>
@@ -49,52 +51,16 @@ void *cntrl_thread( void *arg )
 #else
 int main( void )
 #endif
-{
-    /* Variablen fuer Zeit */
-    zeit_Betriebszustand_t  zeit_absenkung;
-    zeit_event_t            zeit_event;
-    zeit_party_t            zeit_party;
-
-    /* Mittelwertbildung fuer Aussentemperatur */
-    task_tau_t   tau;
-
-    /* Variablen fuer Solarkollektorsteuerung */
-    sol_in_t     sol_in_Sp1, sol_in_Sp2;
-    do_bitbyte_t sol_sp1_av_sb, sol_sp2_av_sb, sol_pu_sb;
-    sol_param_t  sol_par;
-
-    /* Variablen fuer Fussbodenheizung */
-    fb_param_t          fb_par;
-    fb_in_t             fb_in;
-    fb_out_t            fb_out;
-    sup_digreg_coeff_t  fb_q;
-
-    /* Variablen fuer Heizkoerperkreis */
-    hk_param_t          hk_par;
-    hk_in_t             hk_in;
-    hk_out_t            hk_out;
-    sup_digreg_coeff_t  hk_q;
-
-    /* Variablen fuer Warmwasserkreis  */
-    ww_param_t          ww_par;
-    ww_in_t             ww_in;
-    ww_out_t            ww_out;
-    sup_digreg_coeff_t  ww_q;
-
-    /* Variablen fuer Kessel Steuerung */
-    kes_param_t         kes_par; 
-    kes_in_t            kes_in;
-    kes_out_t           kes_out;
-    
+{    
     MUTEX_LOCK();
     param_Init();
-    zeit_Init( &zeit_absenkung, &zeit_event );
-    task_Init( &tau, ALL_Tau_MW );
-    solar_Init( &sol_par );
-    fb_Init( &fb_par, &fb_q, &fb_out );
-    hk_Init( &hk_par, &hk_q, &hk_out );
-    ww_Init( &ww_par, &ww_q, &ww_out );
-    kes_Init( &kes_par, &kes_out );
+    zeit_Init( &cntrl_zeit_absenkung, &cntrl_zeit_event );
+    task_Init( &cntrl_tau, ALL_Tau_MW );
+    solar_Init( &cntrl_sol_par );
+    fb_Init( &cntrl_fb_par, &cntrl_fb_q, &cntrl_fb_out );
+    hk_Init( &cntrl_hk_par, &cntrl_hk_q, &cntrl_hk_out );
+    ww_Init( &cntrl_ww_par, &cntrl_ww_q, &cntrl_ww_out );
+    kes_Init( &cntrl_kes_par, &cntrl_kes_out );
     MUTEX_UNLOCK();
 
 #ifdef __TEST__
@@ -109,62 +75,66 @@ int main( void )
         MUTEX_LOCK();
         /* alles was im Sekunden-, Minuten- und Stundenraster ablaufen muss und *
          * Aussentemperaturmittelwerte ermitteln                                */
-        task_Run( param_all_partydauer, ALL_PARTY, WW_PARTY, ALL_Tau_MW, &tau, &zeit_event, &zeit_party );
+        task_Run( param_all_partydauer, ALL_PARTY, WW_PARTY, ALL_Tau_MW, &cntrl_tau, &cntrl_zeit_event, &cntrl_zeit_party );
         /* Absenkzeiten ermitteln */
-        zeit_Run( &zeit_absenkung, &zeit_event );
+        zeit_Run( &cntrl_zeit_absenkung, &cntrl_zeit_event );
 
-        sol_in_Sp1.koll_t_mw = sol_in_Sp2.koll_t_mw                   = 35.0;  // = SOL_KOLL_T_MW;
-        sol_in_Sp1.sp_to_mw  = ww_in.sol_sp1_to_mw = kes_in.sp1_to_mw = 67.9;  // = SOL_SP1_To_MW; 
-        sol_in_Sp1.sp_tu_mw                        = kes_in.sp1_tu_mw = 34.0;  // = SOL_SP1_Tu_MW;  
-        sol_in_Sp2.sp_to_mw                        = kes_in.sp2_to_mw = 37.0;  // = SOL_SP2_To_MW; 
-        sol_in_Sp2.sp_tu_mw  = ww_in.sol_sp2_tu_mw = kes_in.sp2_tu_mw = 42.0;  // = SOL_SP2_Tu_MW;                     
+        cntrl_sol_in_Sp1.koll_t_mw = cntrl_sol_in_Sp2.koll_t_mw                           = 35.0;  // = SOL_KOLL_T_MW;
+        cntrl_sol_in_Sp1.sp_to_mw  = cntrl_ww_in.sol_sp1_to_mw   = cntrl_kes_in.sp1_to_mw = 67.9;  // = SOL_SP1_To_MW; 
+        cntrl_sol_in_Sp1.sp_tu_mw                                = cntrl_kes_in.sp1_tu_mw = 34.0;  // = SOL_SP1_Tu_MW;  
+        cntrl_sol_in_Sp2.sp_to_mw                                = cntrl_kes_in.sp2_to_mw = 37.0;  // = SOL_SP2_To_MW; 
+        cntrl_sol_in_Sp2.sp_tu_mw  = cntrl_ww_in.sol_sp2_tu_mw   = cntrl_kes_in.sp2_tu_mw = 42.0;  // = SOL_SP2_Tu_MW;                     
 
-        fb_in.tau_mw         = hk_in.tau_mw        = ww_in.tau_mw     = 11.0;   // = ALL_Tau_MW;
-        fb_in.tau_avg        = hk_in.tau_avg       = ww_in.tau_avg    = 13.4;   // = tau.t_36h_mittel;
-        fb_in.sek_tvl_mw                                              = 27.0;   // = FB_SEK_Tvl_MW;
-        fb_in.zustand                                                 = zeit_absenkung.FB_Zustand;
-        fb_in.partytime_flg  = hk_in.partytime_flg                    = zeit_party.all_partytime_flg;
+        cntrl_fb_in.tau_mw         = cntrl_hk_in.tau_mw          = cntrl_ww_in.tau_mw     = 11.0;   // = ALL_Tau_MW;
+        cntrl_fb_in.tau_avg        = cntrl_hk_in.tau_avg         = cntrl_ww_in.tau_avg    = 13.4;   // = tau.t_36h_mittel;
+        cntrl_fb_in.sek_tvl_mw                                                            = 27.0;   // = FB_SEK_Tvl_MW;
+        cntrl_fb_in.zustand        = cntrl_zeit_absenkung.FB_Zustand;
+        cntrl_fb_in.partytime_flg  = cntrl_hk_in.partytime_flg  = cntrl_zeit_party.all_partytime_flg;
 
-        hk_in.tvl_mw         = 45.0;   // = HK_Tvl_MW;
-        hk_in.zustand        = zeit_absenkung.HK_Zustand;
+        cntrl_hk_in.tvl_mw         = 45.0;   // = HK_Tvl_MW;
+        cntrl_hk_in.zustand        = cntrl_zeit_absenkung.HK_Zustand;
 
-        ww_in.tww_mw         = 41.4;   // = WW_Tww_MW;
-        ww_in.hzg_trl_mw     = 31.0;   // = HK_Trl_MW;
+        cntrl_ww_in.tww_mw         = 41.4;   // = WW_Tww_MW;
+        cntrl_ww_in.hzg_trl_mw     = 31.0;   // = HK_Trl_MW;
 
-        kes_in.tvl_mw        = 25.0;   // = KES_Tvl_MW;
-        kes_in.duschzeit     = zeit_absenkung.Duschzeit; 
-        kes_in.partytime_flg = zeit_party.ww_partytime_flg;
+        cntrl_kes_in.tvl_mw        = 25.0;   // = KES_Tvl_MW;
+        cntrl_kes_in.duschzeit     = cntrl_zeit_absenkung.Duschzeit; 
+        cntrl_kes_in.partytime_flg = cntrl_zeit_party.ww_partytime_flg;
 
         /* solar_Run(), fb_Run() und hk_Run() sind unabhaengig von einander */
-        solar_Run( &sol_par, &sol_in_Sp1, &sol_in_Sp2, &sol_sp1_av_sb, &sol_sp2_av_sb, &sol_pu_sb );
-        fb_Run( &fb_par, &fb_q, &fb_in, &fb_out );
-        hk_Run( &hk_par, &fb_q, &hk_in, &hk_out );
+        solar_Run( &cntrl_sol_par, &cntrl_sol_in_Sp1, 
+                   &cntrl_sol_in_Sp2, &cntrl_sol_sp1_av_sb, 
+                   &cntrl_sol_sp2_av_sb, &cntrl_sol_pu_sb );
+        fb_Run( &cntrl_fb_par, &cntrl_fb_q, &cntrl_fb_in, &cntrl_fb_out );
+        hk_Run( &cntrl_hk_par, &cntrl_fb_q, &cntrl_hk_in, &cntrl_hk_out );
 
         /* ww_Run() Eingabewerte sind abhaengig von Ausgabewerten von hk_Run() */
-        ww_in.hk_tvl_sw  = hk_out.tvl_sw;
-        ww_Run( &ww_par, &ww_q, &ww_in, &ww_out );
+        cntrl_ww_in.hk_tvl_sw  = cntrl_hk_out.tvl_sw;
+        ww_Run( &cntrl_ww_par, &cntrl_ww_q, &cntrl_ww_in, &cntrl_ww_out );
         
         /* kes_Run() Eingabewerte abhaengig von Ausgabewerten von hk_Run(), fb_Run() */
-        kes_in.hk_tvl_sw = hk_out.tvl_sw;
-        kes_in.fb_tvl_sw = fb_out.tvl_sw;
-        kes_Run( &kes_par, &kes_in, &kes_out );
+        cntrl_kes_in.hk_tvl_sw = cntrl_hk_out.tvl_sw;
+        cntrl_kes_in.fb_tvl_sw = cntrl_fb_out.tvl_sw;
+        kes_Run( &cntrl_kes_par, &cntrl_kes_in, &cntrl_kes_out );
  
         /* Ab hier Ausgabe des Prozessabbildes */
 #ifdef __TEST__        
-        printf( "CNTRL.C: TEST: ZEIT : Absenkung Fussbodenheizung: %d\n", zeit_absenkung.FB_Zustand );
-        printf( "CNTRL.C: TEST: ZEIT : Absenkung Duschzeit       : %d\n", zeit_absenkung.Duschzeit );
+        printf( "CNTRL.C: TEST: ZEIT : Absenkung Fussbodenheizung: %d\n", cntrl_zeit_absenkung.FB_Zustand );
+        printf( "CNTRL.C: TEST: ZEIT : Absenkung Duschzeit       : %d\n", cntrl_zeit_absenkung.Duschzeit );
         printf( "CNTRL.C: TEST: SOLAR: sp1_av_sb=%d sp2_av_sb=%u sol_pu_sb=%d\n",
-                sol_sp1_av_sb, sol_sp2_av_sb, sol_pu_sb );
+                cntrl_sol_sp1_av_sb, cntrl_sol_sp2_av_sb, cntrl_sol_pu_sb );
         printf( "CNTRL.C: TEST: FB   : tvl_sw=%f prim_mv_y=%f prim_pu_sb=%d sek_pu_sb=%d\n",
-                fb_out.tvl_sw, fb_out.prim_mv_y.y, fb_out.prim_pu_sb, fb_out.sek_pu_sb );
+                cntrl_fb_out.tvl_sw, cntrl_fb_out.prim_mv_y.y, cntrl_fb_out.prim_pu_sb, cntrl_fb_out.sek_pu_sb );
         printf( "CNTRL.C: TEST: HK   : tvl_sw=%f mv_y=%f pu_sb=%d\n",
-                hk_out.tvl_sw, hk_out.mv_y.y, hk_out.pu_sb );        
+                cntrl_hk_out.tvl_sw, cntrl_hk_out.mv_y.y, cntrl_hk_out.pu_sb );        
         printf( "CNTRL.C: TEST: WW   : hzg_tvl_sw=%f hzg_mv_y=%f hzg_pu_y=%f zirk_pu_sb=%d hzg_pu_sb=%d hzg_vv_sb=%d\n", 
-                ww_out.hzg_tvl_sw, ww_out.hzg_mv_y.y, ww_out.hzg_pu_y.y, ww_out.zirk_pu_sb, ww_out.hzg_pu_sb, ww_out.hzg_vv_sb );
+                cntrl_ww_out.hzg_tvl_sw, cntrl_ww_out.hzg_mv_y.y, cntrl_ww_out.hzg_pu_y.y, 
+                cntrl_ww_out.zirk_pu_sb, cntrl_ww_out.hzg_pu_sb, cntrl_ww_out.hzg_vv_sb );
         printf( "CNTRL.C: TEST: KES  : sp1_to_sw=%f sp2_to_sw=%f tvl_sw_sp1=%f tvl_sw_sp2=%f \n",
-                kes_out.sp1_to_sw, kes_out.sp2_to_sw, kes_out.tvl_sw_sp1 , kes_out.tvl_sw_sp2 );
+                cntrl_kes_out.sp1_to_sw, cntrl_kes_out.sp2_to_sw, 
+                cntrl_kes_out.tvl_sw_sp1 , cntrl_kes_out.tvl_sw_sp2 );
         printf( "CNTRL.C: TEST: KES  : tvl_sw=%f pu_sp1_sb=%d pu_sp2_sb=%d\n", 
-                kes_out.tvl_sw, (int)kes_out.pu_sp1_sb, (int)kes_out.pu_sp2_sb );
+                cntrl_kes_out.tvl_sw, cntrl_kes_out.pu_sp1_sb, cntrl_kes_out.pu_sp2_sb );
                 printf( "\n" );        
 #endif /* __TEST__ */
 
