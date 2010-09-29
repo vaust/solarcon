45a5c895916d2873da64d4b4e2aefc846b4ad6c8
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-09-29 06:23:51 +0000

    Störmodul angefangen zu entwickeln.

---
+++ trunk/HeizungRCA_NG/src/err.c
@@ -0,0 +1,15 @@
+#define _ERR_C_
+
+#include "cntrl.h"
+
+/* Pseudocode als Denkhilfe 
+SAMMELSTOERMELDUNG =
+    Kollektorübertemperatur ||
+    Brennermeldung kommt nicht nach max 1 min nach Anforderung ||
+    STB Fussbodenheizung hat ausgelöst ||
+    Kesselstörmeldung ist gesetzt ||
+    
+    ...
+
+
+*/
