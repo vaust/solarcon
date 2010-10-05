c93ed1cd379a8d9dfba7f567fbf6259b770784ef
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-05 11:05:34 +0000

    sup.c wieder in Orginalzustand. Neues C-File sup_awup.c mit Versuch Anti windup zu integrieren.

--- trunk/HeizungRCA_NG/src/sup_awup.c
+++ trunk/HeizungRCA_NG/src/sup_awup.c
@@ -8,7 +8,7 @@ float sup_Limit( float *value, const float lower_limit, const float upper_limit 
     if( *value <= lower_limit ) {
         *value = lower_limit;
     }
-    else if( *value >= upper_limit ) {
+    else if( *value > upper_limit ) {
         *value = upper_limit;
     }
     return( *value );
@@ -26,14 +26,26 @@ float sup_DigRegler( const sup_digreg_coeff_t *q_p,
                      const float               ist, 
                            sup_digreg_out_t   *y_p )
 {
-    float xd; /* xd(t)   */
+    float xd;       /* xd(t) */
 
-    xd   = soll-ist;
-    y_p->y    = y_p->y_1 + q_p->q0*xd + q_p->q1*y_p->xd_1;
+    xd     = soll-ist;
+    y_p->y = y_p->y_1 + q_p->q0*xd + q_p->q1*y_p->xd_1;
+
+    /* Anti Windup Algorithmus */
+    if( y_p->y > q_p->upper_limit )  
+        y_p->y_1 += q_p->antiwup * (y_p->y - q_p->upper_limit);
+    else if( y_p->y < q_p->lower_limit ) 
+        y_p->y_1 += q_p->antiwup * (y_p->y - q_p->lower_limit);
+
     y_p->y_1  = y_p->y;
     y_p->xd_1 = xd;
-    sup_Limit( &(y_p->y  ), q_p->lower_limit, q_p->upper_limit );
-    sup_Limit( &(y_p->y_1), q_p->lower_limit, q_p->upper_limit );
+    sup_Limit( &(y_p->y  ), q_p->lower_limit, q_p->upper_limit );   
+
     return( y_p->y );
+}
+
+void sup_YToAnalog( float scale ,ao_0_10V_t *out_p )
+{
+    *out_p = (ao_0_10V_t) ((scale*VFULLSCALE)/100);
 }
 
