0de738c6b1974c665bbb00a84bc439149bd2c2f2
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-06 08:28:16 +0000

    Anti Windup Implementierung in sup.c als sup_DigRegler2() aufgenommen.

--- trunk/HeizungRCA_NG/src/sup.c
+++ trunk/HeizungRCA_NG/src/sup.c
@@ -52,15 +52,15 @@ float sup_DigRegler2( const sup_digreg_coeff_t *q_p,
     /* x[0] : x Eingangsgrosse  */
     /* x[7] : y Ausgangsgroesse */
     
-    x[0] = soll-ist;
-    x[1] = q_p->kp * x[0];
-    x[2] = q_p->ki * x[0];
-    x[3] = x[2] + x[4] - x[6]; /* x[4] = z^(-1)*x[3] */
-    x[4] = x[3];               /* neuer x[3] ist jetzt x[4] fuer naechsten Durchlauf */
-    x[5] = x[1] + x[3];
-    x[7] = (x[5] > q_p->upper_limit ? q_p->upper_limit : x[5]) < q_p->lower_limit ? q_p->lower_limit : x[5];
-    x[6] = x[5] - x[7];
-    y_p->y = x[7];
+    y_p->x[0] = soll-ist;
+    y_p->x[1] = q_p->kp * y_p->x[0];
+    y_p->x[2] = q_p->ki * y_p->x[0];
+    y_p->x[3] = y_p->x[2] + y_p->x[4] - y_p->x[6]; /* x[4] = z^(-1)*x[3] */
+    y_p->x[4] = y_p->x[3];                         /* neuer x[3] ist jetzt x[4] fuer naechsten Durchlauf */
+    y_p->x[5] = y_p->x[1] + y_p->x[3];
+    y_p->x[7] = (y_p->x[5] > q_p->upper_limit ? q_p->upper_limit : y_p->x[5]) < q_p->lower_limit ? q_p->lower_limit : y_p->x[5];
+    y_p->x[6] = y_p->x[5] - y_p->x[7];
+    y_p->y = y_p->x[7];
     return( y_p->y );
 }
 
