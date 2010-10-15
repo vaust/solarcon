781a7843eb092a9b288b80a3ed1ba38fd7c952bf
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-15 07:39:19 +0000

    schreibe automatisch Version

---
+++ HeizungRCA_NG/writeversion.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import datetime
+import time
+
+Filename_Prefix = str(int(time.time()))
+VERSION = '0.6.5'
+jetzt   = datetime.datetime.now().isoformat(' ')
+ 
+fd = open( './inc/version.h', 'w' )
+fd.write( '#ifndef _VERSION_H_\n' )
+fd.write( '#define _VERSION_H_\n\n' )
+fd.write( '#define VERSIONSTRING "'+VERSION+'\n\tBuild '+jetzt+'\n' )
+fd.write( '\n\#endif\n'
+fd.close()
