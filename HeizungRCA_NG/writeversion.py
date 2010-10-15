275d5a0f4b0dfb3d64a61b630d80c70b9aba0692
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-15 08:37:03 +0000

    Versioninformation Headerfile version.h automatisch erzeugen lassen.

--- HeizungRCA_NG/writeversion.py
+++ HeizungRCA_NG/writeversion.py
@@ -12,6 +12,6 @@ jetzt   = datetime.datetime.now().isoformat(' ')
 fd = open( './inc/version.h', 'w' )
 fd.write( '#ifndef _VERSION_H_\n' )
 fd.write( '#define _VERSION_H_\n\n' )
-fd.write( '#define VERSIONSTRING "'+VERSION+'\n\tBuild '+jetzt+'\n' )
-fd.write( '\n\#endif\n'
+fd.write( '#define VERSIONSTRING "'+VERSION+'\\n\\tBuild '+jetzt+'\\n"' )
+fd.write( '\n\n#endif\n' )
 fd.close()
