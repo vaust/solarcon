368dbef1e28fa4d2955cc633cab9c0f4023a0733
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-16 17:31:43 +0000

    AUTO/HAND Betriebssteuerung in telnet.c weiter verbessert. Schwachlaststeuerung dürfte bei Regler mit Antiwindup nicht notwendig sein.

--- HeizungRCA_NG/inc/version.h
+++ HeizungRCA_NG/inc/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.1\n\tBuild 2010-10-15 18:52:15.331505\n"
+#define VERSIONSTRING "0.7.1\n\tBuild 2010-10-16 18:59:10.848653\n"
 
 #endif
