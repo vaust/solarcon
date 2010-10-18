db442aa32e45286c008d10dc26b6557919a7f5f7
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-18 12:22:14 +0000

    Fehlermeldungsmodul err.c integriert.

--- trunk/HeizungRCA_NG/src/err.c
+++ trunk/HeizungRCA_NG/src/err.c
@@ -1,6 +1,6 @@
 #define _ERR_C_
 
-#include "cntrl.h"
+#include "err.h"
 
 /* 
 SAMMELSTOERMELDUNG =
@@ -13,3 +13,37 @@ SAMMELSTOERMELDUNG =
 
 
 */
+
+void err_Init( err_param_t *par_p )
+{
+    par_p->br_TimeOut = 240;    /* 4 min */
+}
+
+/** err_Run(): Betriebszustaende lesen und auf Plausibiliteat prufen.
+    Falls nicht plausibel: Sammelstoermeldung blinken lassen
+  */
+void err_Run( const err_param_t *par_p,
+              const err_in_t    *in_p,
+                    err_out_t   *out_p )
+{
+    if( (in_p->kes_tvl_sw    >  in_p->kes_tvl_mw ) && 
+        (out_p->br_Countdown >= par_p->br_TimeOut)    ) {
+        if( in_p->br_RueckMeldung == RESET ) 
+            if( br_Countdown > 0 ) br_Countdown --;
+        else 
+            br_Countdown = par_p->br_TimeOut;
+    }
+    else {
+        out_p->br_Countdown = par_p->br_TimeOut; 
+    }
+    
+    if( (0    == br_Countdown              ) ||
+        (SET  == in_p->br_StoerMeldung     ) ||
+        (SET  == in_p->stb_Fussbodenheizung)    ) {
+     
+        out_p->Sammelstoermeldung = SET;
+    }
+}
+        
+    
+         
