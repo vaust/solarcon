1a98a5084e0f441ecb8254579136c814174bc895
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-18 09:30:54 +0000

    Versionsstringgenerierung toller

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.1\n\tBuild 1287393196\n"
+#define VERSIONSTRING "0.7.1\n\tBuild 1287394188\n\t2010-10-18 11:29:48.531000\n"
 
 #endif
