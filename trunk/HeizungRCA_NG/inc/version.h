9c50d4047487564d07a7bb9f9f1a7cc518ad5b95
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-29 07:00:12 +0000

    Versionstring besser lesbar

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.2\n\tBuild 278509 2010-10-28T17:08:29.870461\n"
+#define VERSIONSTRING "0.7.2\n\tBuild 35575 2010-10-29 08:59:35\n"
 
 #endif
