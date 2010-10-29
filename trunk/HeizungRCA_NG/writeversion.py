1028c0dd03f2342240fbcb35186dfbc6bcd62c08
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-29 08:24:07 +0000

    Versionanzeige jetzt mit SVN Revision um genau den Stand nachvollziehbar zu machen.

--- trunk/HeizungRCA_NG/writeversion.py
+++ trunk/HeizungRCA_NG/writeversion.py
@@ -6,16 +6,16 @@ import datetime
 import time
 
 SVN     = '$Revision$'
+SVN_URL = '$HeadURL$'
 SVN_REV = SVN.split(':')[1].split('$')[0]
 VERSION = '0.7.2 SVN:'+SVN_REV
 now = datetime.datetime.now()
 now = datetime.datetime( now.year, now.month, now.day, now.hour, now.minute, now.second )
-jetzt1  = now.isoformat(' ')
-jetzt2  = str(int(time.time()) % 100000)
+jetzt = now.isoformat(' ')
 
 fd = open( './inc/version.h', 'w' )
 fd.write( '#ifndef _VERSION_H_\n' )
 fd.write( '#define _VERSION_H_\n\n' )
-fd.write( '#define VERSIONSTRING "'+VERSION+'\\n\\tBuild '+jetzt2+' '+jetzt1+'\\n"' )
+fd.write( '#define VERSIONSTRING "'+VERSION+'\\n\\tBuild '+jetzt+'\\n"' )
 fd.write( '\n\n#endif\n' )
 fd.close()
