166dda1a80842a67661835375ecbb8fb570cfa6b
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-29 06:14:58 +0000

    Hilfetext erweitert und umstrukturiert. io_v2 weiterentwickelt - noch nicht integrierbar.

--- HeizungRCA_NG/inc/io_v2.h
+++ HeizungRCA_NG/inc/io_v2.h
@@ -16,18 +16,24 @@ typedef enum io_obj_status_e { obj_Normal,             /**< Sensorwert ist inner
 typedef io_tempsens_obj_s {
     float           messbereich_anfang;
     float           messbereich_ende;
-    float           aufloesung;
+    // float           aufloesung;
     float           messwert;
     io_obj_status_t status;
     temp10_pt1000_t *kbus_adresse_p;
 } io_tempsens_obj_t;
+
+typedef io_digin_obj_s {
+    di_bitbyte_t    messwert;
+    io_obj_status_t status;
+} io_digin_obj_t;
+    
 /* </Typen> */
 
 /* <Variablen> */
 #ifdef _IO_V2_C_
     #define PUBLIC
 #else
-    #define PUBLIC extern
+    #define PUBLIC extern const
 #endif
 
 PUBLIC io_tempsens_obj_t    io_ALL_Tau_MW;
@@ -48,8 +54,8 @@ PUBLIC io_tempsens_obj_t    io_WW_Tww_MW;
 /* </Variablen> */
 
 /* <Prototypen> */
-void io_InitAlleTemperaturSensoren( void );
-float io_MesseTemperatur( io_tempsens_obj_t *this );
+void io_InitAllTempSens( void );
+float io_Temp( io_tempsens_obj_t *this );
 /* </Prototypen> */
 
 #endif /* _IO_V2_H_ */
