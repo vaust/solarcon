fe8a46afb33c63c82cec5ad2fa3139bdebd179b3
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-10-29 11:00:21 +0000

    Entwicklungspfad ermitteln.

--- HeizungRCA_NG/writeversion.py
+++ HeizungRCA_NG/writeversion.py
@@ -9,6 +9,7 @@ SVN     = '$Revision$'
 SVN_URL = '$HeadURL$'
 SVN_REV = SVN.split(':')[1].split('$')[0]
 VERSION = '0.7.2 SVN:'+SVN_REV
+DEVPATH = SVN_URL.split('.com')[1].split('/write')[0]
 now = datetime.datetime.now()
 now = datetime.datetime( now.year, now.month, now.day, now.hour, now.minute, now.second )
 jetzt = now.isoformat(' ')
@@ -16,6 +17,7 @@ jetzt = now.isoformat(' ')
 fd = open( './inc/version.h', 'w' )
 fd.write( '#ifndef _VERSION_H_\n' )
 fd.write( '#define _VERSION_H_\n\n' )
-fd.write( '#define VERSIONSTRING "'+VERSION+'\\n\\tBuild '+jetzt+'\\n"' )
-fd.write( '\n\n#endif\n' )
+fd.write( '#define VERSIONSTRING "'+VERSION+'\\n\\tBuild '+jetzt+'\\n"\n' )
+fd.write( '#define VERSIONDEVPATH "'+DEVPATH+'"\n' )
+fd.write( '\n#endif\n' )
 fd.close()
