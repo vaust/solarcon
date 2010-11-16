8ee6894b7e41782556bc6aa8baafa2d07c3205f2
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-11-16 12:59:22 +0000

    Unbenutzte Strukturelemente auskommentiert in kes.c / kes.h / telnet_vars.h

--- HeizungRCA_NG/inc/kes.h
+++ HeizungRCA_NG/inc/kes.h
@@ -5,23 +5,23 @@
 
 /* <Typen> */
 typedef struct {
-    float TA;
-    float tvl_absenk;
-    float tvl_steigung;
-    float tvl_niveau;
-    float tvl_min;
-    float tvl_max;
-    float at_start;
-    float frostschutz;
+//    float TA;
+//    float tvl_absenk;
+//    float tvl_steigung;
+//    float tvl_niveau;
+//    float tvl_min;
+//    float tvl_max;
+//    float at_start;
+//    float frostschutz;
     float sp_dt_sw;                 /**< Globaler Wert div 2 !! */
     float ww_tww_sw;
 } kes_param_t;
 
 typedef struct {
     float        sp1_to_mw;
-    float        sp1_tu_mw;
+//    float        sp1_tu_mw;
     float        sp2_to_mw;
-    float        sp2_tu_mw;
+//    float        sp2_tu_mw;
     float        tvl_mw;
     u32_t        gz_mw;         /**< Gaszaehlerstand (noch nicht in Verwendung */
     float        hk_tvl_sw;     
