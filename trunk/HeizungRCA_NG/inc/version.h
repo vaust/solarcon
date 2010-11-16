16e25a14faad0237e9d8747d6e9a8d1fe028cfb8
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-11-16 07:30:31 +0000

    aktuelles Binary für WAGO Steuerung

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.2 SVN: 277 \n\tBuild 2010-11-04 10:20:24\n"
+#define VERSIONSTRING "0.7.2 SVN: 284 \n\tBuild 2010-11-16 08:28:38\n"
 #define VERSIONDEVPATH "/svn/trunk/HeizungRCA_NG\n"
 
 #endif
