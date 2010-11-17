2300580c1104e17a40a524d12d17161530cc6290
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-11-17 13:40:53 +0000

    Fehler in Hilfetext korrigiert.

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.2 SVN: 309 \n\tBuild 2010-11-17 10:36:06\n"
+#define VERSIONSTRING "0.7.2 SVN: 310 \n\tBuild 2010-11-17 14:24:33\n"
 #define VERSIONDEVPATH "/svn/trunk/HeizungRCA_NG\n"
 
 #endif
