cf6eb6d9c20874d01519a726b84d3fd57879b74d
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2010-11-17 07:11:01 +0000

    Versuch Systemintervalltimerfunktion setitimer() zu nutzen, um Echtzeit zu gewährleisten.

--- HeizungRCA_NG/src/cntrl.c
+++ HeizungRCA_NG/src/cntrl.c
@@ -41,27 +41,16 @@
 #include <semaphore.h>
 #define MUTEX_LOCK()    pthread_mutex_lock( &mutex )
 #define MUTEX_UNLOCK()  pthread_mutex_unlock( &mutex )
+extern pthread_mutex_t  mutex;
 #else
 #define MUTEX_LOCK()
 #define MUTEX_UNLOCK()
 #endif
 
-// #ifdef __REENTRANT__
-// void terminate( int sig );
+void terminate( int sig );
 
-// extern pthread_mutex_t  mutex;
-
-// void *cntrl_thread( void *arg )
-// #else
-// int main( void )
-// #endif
-
-extern pthread_mutex_t  mutex;
-
-void cntrl_main( int sig )
+void cntrl_open( void )
 {
-    static int counter = 0;
-    
     KBUSOPEN();
     KBUSUPDATE();
 
@@ -91,138 +80,137 @@ void cntrl_main( int sig )
     cntrl_mdl_aktiv.inp_err_aktiv = SET;
     MUTEX_UNLOCK();
 
-//    while( 1  ) {
-        MUTEX_LOCK();
-        /*----------- Prozessabbild aktualisieren -----------*/
-        KBUSUPDATE();
+    KBUSUPDATE();
+}
 
-        /*---------- EINGABE DES PROZESSABBILDES ------------*/
-        /* alles was im Sekunden-, Minuten- und Stundenraster ablaufen muss und *
-         * Aussentemperaturmittelwerte ermitteln                                */
-        task_Run( param_all_partydauer,
-                  io_get_ALL_PARTY(),
-                  io_get_WW_PARTY(),
-                  io_get_ALL_Tau_MW(),
-                  &cntrl_tau, &cntrl_zeit_event, &cntrl_zeit_party );
-        /* Absenkzeiten ermitteln */
-        zeit_Run( &cntrl_zeit_absenkung, &cntrl_zeit_event );
+void cntrl_main( int sig )
+{
+    static int counter = 0;
+    
+    MUTEX_LOCK();
+    /*----------- Prozessabbild aktualisieren -----------*/
+    KBUSUPDATE();
 
-        /* Prozessdaten */
-        if( SET == cntrl_mdl_aktiv.inp_sol_aktiv ) {
-            cntrl_sol_in.koll_t_mw[KO1]  = io_get_SOL_KOLL_T_MW();
-            cntrl_sol_in.t_sp[SP1].to_mw = io_get_SOL_SP1_To_MW();
-            cntrl_sol_in.t_sp[SP1].tu_mw = io_get_SOL_SP1_Tu_MW();
-            cntrl_sol_in.t_sp[SP2].to_mw = io_get_SOL_SP2_To_MW();
-            cntrl_sol_in.t_sp[SP2].tu_mw = io_get_SOL_SP2_Tu_MW();
-        }
-        if( SET == cntrl_mdl_aktiv.inp_fb_aktiv ) {
-            cntrl_fb_in.tau_mw           = io_get_ALL_Tau_MW();
-            cntrl_fb_in.sek_tvl_mw       = io_get_FB_SEK_Tvl_MW();
-            cntrl_fb_in.tau_avg          = cntrl_tau.t_36h_mittel;
-            cntrl_fb_in.zustand          = cntrl_zeit_absenkung.FB_Zustand;
-            cntrl_fb_in.partytime_flg    = cntrl_zeit_party.all_partytime_flg;
-        }
-        if( SET == cntrl_mdl_aktiv.inp_hk_aktiv ) {
-            cntrl_hk_in.tau_mw           = io_get_ALL_Tau_MW();
-            cntrl_hk_in.tvl_mw           = io_get_HK_Tvl_MW();
-            cntrl_hk_in.tau_avg          = cntrl_tau.t_36h_mittel;
-            cntrl_hk_in.zustand          = cntrl_zeit_absenkung.HK_Zustand;
-            cntrl_hk_in.partytime_flg    = cntrl_zeit_party.all_partytime_flg;
-        }
-        if( SET == cntrl_mdl_aktiv.inp_ww_aktiv ) {
-            cntrl_ww_in.hzg_tvl_mw       = io_get_HK_Tvl_MW();
-            cntrl_ww_in.sp1_to_mw        = io_get_SOL_SP1_Tu_MW();
-            cntrl_ww_in.sp2_tu_mw        = io_get_SOL_SP2_Tu_MW();
-            cntrl_ww_in.tau_mw           = io_get_ALL_Tau_MW();
-            cntrl_ww_in.tau_avg          = cntrl_tau.t_36h_mittel;
-            cntrl_ww_in.tww_mw           = io_get_WW_Tww_MW();
-            cntrl_ww_in.hzg_trl_mw       = io_get_HK_Trl_MW();
-            cntrl_ww_in.zirkzustand      = cntrl_zeit_absenkung.Zirk_Zustand;
-            cntrl_ww_in.duschzeit        = cntrl_zeit_absenkung.Duschzeit;
-        }
-        if( SET == cntrl_mdl_aktiv.inp_kes_aktiv ) {
-            cntrl_kes_in.sp1_to_mw       = io_get_SOL_SP1_To_MW();
-            cntrl_kes_in.sp2_to_mw       = io_get_SOL_SP2_To_MW();
-            cntrl_kes_in.tvl_mw          = io_get_KES_Tvl_MW();
-            cntrl_kes_in.br_bm           = io_get_KES_BR_BM();
-            cntrl_kes_in.duschzeit       = cntrl_zeit_absenkung.Duschzeit;
-        }
-        if( SET == cntrl_mdl_aktiv.inp_err_aktiv ) {
-            cntrl_err_in.br_RueckMeldung      = io_get_KES_BR_BM();
-            cntrl_err_in.br_StoerMeldung      = io_get_KES_SSM();
-            cntrl_err_in.kes_tvl_mw           = io_get_KES_Tvl_MW();
-            cntrl_err_in.stb_Fussbodenheizung = io_get_FB_SEK_TW();
-        }
-        /*---------- VERARBEITUNG DES PROZESSABBILDES -----------*/
-        /* solar_Run(), fb_Run() und hk_Run() sind unabhaengig von einander */
-        if( SET == cntrl_mdl_aktiv.sol_aktiv )
-            cntrl_err_in.sol_err = sol_Run( &cntrl_sol_par, &cntrl_sol_in, &cntrl_sol_out );
-        if( SET == cntrl_mdl_aktiv.fb_aktiv )
-            fb_Run( &cntrl_fb_par, &cntrl_fb_q, &cntrl_fb_in, &cntrl_fb_out );
-        if( SET == cntrl_mdl_aktiv.hk_aktiv )
-            hk_Run( &cntrl_hk_par, &cntrl_hk_q, &cntrl_hk_in, &cntrl_hk_out );
+    /*---------- EINGABE DES PROZESSABBILDES ------------*/
+    /* alles was im Sekunden-, Minuten- und Stundenraster ablaufen muss und *
+     * Aussentemperaturmittelwerte ermitteln                                */
+    task_Run( param_all_partydauer,
+              io_get_ALL_PARTY(),
+              io_get_WW_PARTY(),
+              io_get_ALL_Tau_MW(),
+              &cntrl_tau, &cntrl_zeit_event, &cntrl_zeit_party );
+    /* Absenkzeiten ermitteln */
+    zeit_Run( &cntrl_zeit_absenkung, &cntrl_zeit_event );
 
-        /* ww_Run() Eingabewerte sind abhaengig von Ausgabewerten von hk_Run() */
-        if( SET == cntrl_mdl_aktiv.ww_aktiv ) {
-            cntrl_ww_in.hk_tvl_sw  = cntrl_hk_out.tvl_sw;
-            ww_Run( &cntrl_ww_par, &cntrl_ww_q, &cntrl_ww_in, &cntrl_ww_out );
-        }
+    /* Prozessdaten */
+    if( SET == cntrl_mdl_aktiv.inp_sol_aktiv ) {
+        cntrl_sol_in.koll_t_mw[KO1]  = io_get_SOL_KOLL_T_MW();
+        cntrl_sol_in.t_sp[SP1].to_mw = io_get_SOL_SP1_To_MW();
+        cntrl_sol_in.t_sp[SP1].tu_mw = io_get_SOL_SP1_Tu_MW();
+        cntrl_sol_in.t_sp[SP2].to_mw = io_get_SOL_SP2_To_MW();
+        cntrl_sol_in.t_sp[SP2].tu_mw = io_get_SOL_SP2_Tu_MW();
+    }
+    if( SET == cntrl_mdl_aktiv.inp_fb_aktiv ) {
+        cntrl_fb_in.tau_mw           = io_get_ALL_Tau_MW();
+        cntrl_fb_in.sek_tvl_mw       = io_get_FB_SEK_Tvl_MW();
+        cntrl_fb_in.tau_avg          = cntrl_tau.t_36h_mittel;
+        cntrl_fb_in.zustand          = cntrl_zeit_absenkung.FB_Zustand;
+        cntrl_fb_in.partytime_flg    = cntrl_zeit_party.all_partytime_flg;
+    }
+    if( SET == cntrl_mdl_aktiv.inp_hk_aktiv ) {
+        cntrl_hk_in.tau_mw           = io_get_ALL_Tau_MW();
+        cntrl_hk_in.tvl_mw           = io_get_HK_Tvl_MW();
+        cntrl_hk_in.tau_avg          = cntrl_tau.t_36h_mittel;
+        cntrl_hk_in.zustand          = cntrl_zeit_absenkung.HK_Zustand;
+        cntrl_hk_in.partytime_flg    = cntrl_zeit_party.all_partytime_flg;
+    }
+    if( SET == cntrl_mdl_aktiv.inp_ww_aktiv ) {
+        cntrl_ww_in.hzg_tvl_mw       = io_get_HK_Tvl_MW();
+        cntrl_ww_in.sp1_to_mw        = io_get_SOL_SP1_Tu_MW();
+        cntrl_ww_in.sp2_tu_mw        = io_get_SOL_SP2_Tu_MW();
+        cntrl_ww_in.tau_mw           = io_get_ALL_Tau_MW();
+        cntrl_ww_in.tau_avg          = cntrl_tau.t_36h_mittel;
+        cntrl_ww_in.tww_mw           = io_get_WW_Tww_MW();
+        cntrl_ww_in.hzg_trl_mw       = io_get_HK_Trl_MW();
+        cntrl_ww_in.zirkzustand      = cntrl_zeit_absenkung.Zirk_Zustand;
+        cntrl_ww_in.duschzeit        = cntrl_zeit_absenkung.Duschzeit;
+    }
+    if( SET == cntrl_mdl_aktiv.inp_kes_aktiv ) {
+        cntrl_kes_in.sp1_to_mw       = io_get_SOL_SP1_To_MW();
+        cntrl_kes_in.sp2_to_mw       = io_get_SOL_SP2_To_MW();
+        cntrl_kes_in.tvl_mw          = io_get_KES_Tvl_MW();
+        cntrl_kes_in.br_bm           = io_get_KES_BR_BM();
+        cntrl_kes_in.duschzeit       = cntrl_zeit_absenkung.Duschzeit;
+    }
+    if( SET == cntrl_mdl_aktiv.inp_err_aktiv ) {
+        cntrl_err_in.br_RueckMeldung      = io_get_KES_BR_BM();
+        cntrl_err_in.br_StoerMeldung      = io_get_KES_SSM();
+        cntrl_err_in.kes_tvl_mw           = io_get_KES_Tvl_MW();
+        cntrl_err_in.stb_Fussbodenheizung = io_get_FB_SEK_TW();
+    }
+    /*---------- VERARBEITUNG DES PROZESSABBILDES -----------*/
+    /* solar_Run(), fb_Run() und hk_Run() sind unabhaengig von einander */
+    if( SET == cntrl_mdl_aktiv.sol_aktiv )
+        cntrl_err_in.sol_err = sol_Run( &cntrl_sol_par, &cntrl_sol_in, &cntrl_sol_out );
+    if( SET == cntrl_mdl_aktiv.fb_aktiv )
+        fb_Run( &cntrl_fb_par, &cntrl_fb_q, &cntrl_fb_in, &cntrl_fb_out );
+    if( SET == cntrl_mdl_aktiv.hk_aktiv )
+        hk_Run( &cntrl_hk_par, &cntrl_hk_q, &cntrl_hk_in, &cntrl_hk_out );
 
-        /* kes_Run() Eingabewerte abhaengig von Ausgabewerten von hk_Run() und fb_Run() */
-        if( SET == cntrl_mdl_aktiv.kes_aktiv ) {
-            cntrl_kes_in.hk_tvl_sw = cntrl_hk_out.tvl_sw;
-            cntrl_kes_in.fb_tvl_sw = cntrl_fb_out.tvl_sw;
-            kes_Run( &cntrl_kes_par, &cntrl_kes_in, &cntrl_kes_out );
-        }
+    /* ww_Run() Eingabewerte sind abhaengig von Ausgabewerten von hk_Run() */
+    if( SET == cntrl_mdl_aktiv.ww_aktiv ) {
+        cntrl_ww_in.hk_tvl_sw  = cntrl_hk_out.tvl_sw;
+        ww_Run( &cntrl_ww_par, &cntrl_ww_q, &cntrl_ww_in, &cntrl_ww_out );
+    }
 
-        /* Sammelstoermeldung bedienen */
-        if( SET == cntrl_mdl_aktiv.err_aktiv ) {
-            cntrl_err_in.kes_tvl_sw = cntrl_kes_out.tvl_sw;
-            err_Run( &cntrl_err_par, &cntrl_err_in, &cntrl_err_out );
-        }
+    /* kes_Run() Eingabewerte abhaengig von Ausgabewerten von hk_Run() und fb_Run() */
+    if( SET == cntrl_mdl_aktiv.kes_aktiv ) {
+        cntrl_kes_in.hk_tvl_sw = cntrl_hk_out.tvl_sw;
+        cntrl_kes_in.fb_tvl_sw = cntrl_fb_out.tvl_sw;
+        kes_Run( &cntrl_kes_par, &cntrl_kes_in, &cntrl_kes_out );
+    }
 
-        /*---------- AUSGABE DES PROZESSABBILDES ------------*/
-        io_put_SOL_PU_SB( cntrl_sol_out.pu_sb[KO1] );
-        io_put_SOL_SP1_AV_SB( cntrl_sol_out.av_sb[SP1] );
-        io_put_SOL_SP2_AV_SB( cntrl_sol_out.av_sb[SP2] );
+    /* Sammelstoermeldung bedienen */
+    if( SET == cntrl_mdl_aktiv.err_aktiv ) {
+        cntrl_err_in.kes_tvl_sw = cntrl_kes_out.tvl_sw;
+        err_Run( &cntrl_err_par, &cntrl_err_in, &cntrl_err_out );
+    }
 
-        io_put_FB_PRIM_MV_Y( cntrl_fb_out.prim_mv_y.y );
-        io_put_FB_PRIM_PU_SB( cntrl_fb_out.prim_pu_sb );
-        io_put_FB_SEK_PU_SB( cntrl_fb_out.sek_pu_sb );
+    /*---------- AUSGABE DES PROZESSABBILDES ------------*/
+    io_put_SOL_PU_SB( cntrl_sol_out.pu_sb[KO1] );
+    io_put_SOL_SP1_AV_SB( cntrl_sol_out.av_sb[SP1] );
+    io_put_SOL_SP2_AV_SB( cntrl_sol_out.av_sb[SP2] );
 
-        io_put_HK_MV_Y( cntrl_hk_out.mv_y.y );
-        io_put_HK_PU_SB( cntrl_hk_out.pu_sb );
+    io_put_FB_PRIM_MV_Y( cntrl_fb_out.prim_mv_y.y );
+    io_put_FB_PRIM_PU_SB( cntrl_fb_out.prim_pu_sb );
+    io_put_FB_SEK_PU_SB( cntrl_fb_out.sek_pu_sb );
 
-        io_put_WW_HZG_MV_Y( cntrl_ww_out.hzg_mv_y.y );
-        io_put_WW_HZG_VV_SB( cntrl_ww_out.hzg_vv_sb );
-        io_put_WW_HZG_PU_SB( cntrl_ww_out.hzg_pu_sb );
-        io_put_WW_HZG_PU_Y( cntrl_ww_out.hzg_pu_y.y );
-        io_put_WW_ZIRK_PU_SB( cntrl_ww_out.zirk_pu_sb );
+    io_put_HK_MV_Y( cntrl_hk_out.mv_y.y );
+    io_put_HK_PU_SB( cntrl_hk_out.pu_sb );
 
-        io_put_KES_Tvl_Y( cntrl_kes_out.tvl_sw );
-        io_put_KES_PU_SP1_SB( cntrl_kes_out.pu_sp1_sb );
-        io_put_KES_PU_SP2_SB( cntrl_kes_out.pu_sp2_sb );
+    io_put_WW_HZG_MV_Y( cntrl_ww_out.hzg_mv_y.y );
+    io_put_WW_HZG_VV_SB( cntrl_ww_out.hzg_vv_sb );
+    io_put_WW_HZG_PU_SB( cntrl_ww_out.hzg_pu_sb );
+    io_put_WW_HZG_PU_Y( cntrl_ww_out.hzg_pu_y.y );
+    io_put_WW_ZIRK_PU_SB( cntrl_ww_out.zirk_pu_sb );
 
-        /* Lebenszeichen der Steuerung */
-        io_put_CONTROL_AKTIV( !io_get_CONTROL_AKTIV() );
+    io_put_KES_Tvl_Y( cntrl_kes_out.tvl_sw );
+    io_put_KES_PU_SP1_SB( cntrl_kes_out.pu_sp1_sb );
+    io_put_KES_PU_SP2_SB( cntrl_kes_out.pu_sp2_sb );
 
-        if( cntrl_err_out.Sammelstoermeldung == RESET ) {
-            io_put_STOERUNG( IO_AUS );              /* Stoermeldung AUS */
-        }
-        else {
-            io_put_STOERUNG( !io_get_STOERUNG() );  /* Stoermeldung blinken lassen */
-        }
+    /* Lebenszeichen der Steuerung */
+    io_put_CONTROL_AKTIV( !io_get_CONTROL_AKTIV() );
 
-        /*---------- Prozessabbild aktualisieren ----------*/
-        KBUSUPDATE();
-        MUTEX_UNLOCK();
+    if( cntrl_err_out.Sammelstoermeldung == RESET ) {
+        io_put_STOERUNG( IO_AUS );              /* Stoermeldung AUS */
+    }
+    else {
+        io_put_STOERUNG( !io_get_STOERUNG() );  /* Stoermeldung blinken lassen */
+    }
 
-        /* Abtastzeit abwarten. ACHTUNG: Rechenzeit nicht beruecksichtigt.
-           Muesste eigentlich ueber Timerfunktion laufen */
-        // SLEEP( ABTASTZEIT_USEC );
-//    }
-
-    KBUSCLOSE();
+    /*---------- Prozessabbild aktualisieren ----------*/
+    KBUSUPDATE();
+    MUTEX_UNLOCK();
     
     counter ++;
     printf( "CNTRL.C: counter = %d\n", counter );
@@ -230,3 +218,9 @@ void cntrl_main( int sig )
     return( 0 );
 }
 
+void cntrl_close( void )
+{
+    KBUSCLOSE();
+}
+ 
+    
