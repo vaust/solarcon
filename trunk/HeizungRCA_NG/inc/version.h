e6c8f7b063d275936a9581bc31c92f61071f02af
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2011-01-05 07:25:43 +0000

    Binaries für WAGO Steuerung und PC Test

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.4\n\tBuild 2011-01-05 07:32:50\n"
+#define VERSIONSTRING "0.7.4\n\tBuild 2011-01-05 07:52:07\n"
 #define VERSIONDEVPATH "/svn/trunk/HeizungRCA_NG\n"
 
 #endif
