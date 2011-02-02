d695f92322e1e2b7a5da415257ff68af14822749
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2011-02-02 07:20:27 +0000

    Auf WAGO Steuerung zuhause getestet. Stand vor Kapselung der Schaltzeiten in zeit.h

---
+++ tags/HeizungRCA_NG_20110202/inc/hk.h
@@ -0,0 +1,52 @@
+#ifndef _HK_H_
+#define _HK_H_
+
+#include "sup.h"
+
+/* <Typen> */
+typedef struct {
+    float reg_kp;
+    float reg_ap;
+    float reg_ki;
+    float TA;
+    float tvl_absenk;
+    float tvl_steigung;
+    float tvl_niveau;
+    float tvl_min;
+    float tvl_max;
+    float at_start;
+    float frostschutz;
+    float tr_sw;
+} hk_param_t;
+
+typedef struct {
+    float        tau_mw;
+    float        tau_avg;
+    float        tvl_mw;
+    abgesenkt_t  zustand;
+    u8_t         partytime_flg;
+} hk_in_t;
+
+typedef struct {
+    float            tvl_sw;
+    sup_digreg_out_t mv_y;
+    do_bitbyte_t     pu_sb;
+} hk_out_t;
+/* <Typen/> */
+
+/* <Prototypen> */
+void hk_Init( hk_param_t *par_p, sup_digreg_coeff_t *q_p, hk_out_t *out_p );
+void hk_Run( const hk_param_t         *par_p, 
+             const sup_digreg_coeff_t *q_p, 
+             const hk_in_t            *in_p, 
+                   hk_out_t           *out_p );
+void hk_WriteInp(       hk_in_t     *in_p,
+                  const float        tau_mw,
+                  const float        tau_avg,
+                  const float        tvl_mw,
+                  const abgesenkt_t  zustand,
+                  const u8_t         partytime_flg );
+/* <Prototypen/> */
+
+#endif /* _HK_H_ */
+
