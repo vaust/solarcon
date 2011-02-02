5e6f16e351102428fd58ebb7f0d4d8f6f829c83c
Volker.Stegmann <volker.stegmann@googlemail.com>
2011-02-02 06:38:22 +0000

    Feiertagsfehler getestet.

--- HeizungRCA_NG/inc/version.h
+++ HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.4\n\tBuild 2011-01-05 07:52:07\n"
+#define VERSIONSTRING "0.7.4\n\tBuild 2011-02-02 07:06:44\n"
 #define VERSIONDEVPATH "/svn/trunk/HeizungRCA_NG\n"
 
 #endif
