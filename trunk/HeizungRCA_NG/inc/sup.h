ebfa41feb0256ee744ae4027fc8d4817f8ab0caf
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2011-02-15 06:31:02 +0000

    Prototyp von sup_Limit() korrigiert.

--- trunk/HeizungRCA_NG/inc/sup.h
+++ trunk/HeizungRCA_NG/inc/sup.h
@@ -25,7 +25,7 @@ typedef struct sup_digreg_out_s {
 /* <Typen> */
 
 /* <Prototypen> */
-float sup_Limit( float *value, const float lower_limit, const float upper_limit );
+float sup_Limit( float value, const float lower_limit, const float upper_limit );
 void sup_DigRegInit( sup_digreg_coeff_t *q_p, 
                      sup_digreg_out_t   *y_p,
                      float               TA,
