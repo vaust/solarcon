3288d7b3bd86349a6265f43403a44c8799ce570a
volker.stegmann@googlemail.com <volker.stegmann@googlemail.com@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2011-03-02 11:59:05 +0000

    err.c an OOP angepasst. -r Diese und die folgenden Zeilen werden ignoriert --
    M    HeizungRCA_NG/test
    M    HeizungRCA_NG/src/telnet.c
    M    HeizungRCA_NG/writeversion.py
    M    HeizungRCA_NG/inc/version.h
    M    HeizungRCA_NG/Makefile

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.4\n\tBuild 2011-02-02 07:06:44\n"
+#define VERSIONSTRING "0.7.6\n\tBuild 2011-03-02 12:57:53\n"
 #define VERSIONDEVPATH "/svn/trunk/HeizungRCA_NG\n"
 
 #endif
