1ed4221847cc009ecc889753bad38c9f73c8545e
Volker.Stegmann <volker.stegmann@googlemail.com>
2011-03-07 18:51:23 +0000

    Version auf 0.8 geandert.

--- HeizungRCA_NG/inc/version.h
+++ HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.7.6\n\tBuild 2011-03-02 12:57:53\n"
+#define VERSIONSTRING "0.7.6\n\tBuild 2011-03-04 08:07:33\n"
 #define VERSIONDEVPATH "/svn/trunk/HeizungRCA_NG\n"
 
 #endif
