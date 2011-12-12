076e8b6fc78d7aa54cd78849bff9b3b35e422f0d
volker.stegmann@googlemail.com <volker.stegmann@googlemail.com@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2011-12-12 17:45:47 +0000

    Scrollbalken und erstes Label auf Bild platziert. Scrollen verschiebt aber Bild unter dem Label??!!

--- trunk/Visualisierung/Tk_Visual.py
+++ trunk/Visualisierung/Tk_Visual.py
@@ -1,23 +1,29 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+
 def cllbck(event):
-    print('Mouse clicked at ', event.x, event.y)
+    print('Mouse clicked at ', main.canvasx(event.x), main.canvasy(event.y) )
 
 root = tk.Tk()
 bgpic = tk.PhotoImage(file='Visualisierung.gif')
-main_frame = tk.Canvas(root, bg='black', width=800, height=800, scrollregion=(-1000,-1000,1000,1000))
-main_frame.bind('<Button-1>', cllbck)
-main_frame.create_image(0,0, image=bgpic)
 
-right_scrlbar = ttk.Scrollbar( root, orient=tk.VERTICAL)
-bottom_scrlbar = ttk.Scrollbar( root, orient=tk.HORIZONTAL)
+vbar = ttk.Scrollbar( root, orient=tk.VERTICAL)
+hbar = ttk.Scrollbar( root, orient=tk.HORIZONTAL)
+main = tk.Canvas(root, bg='white', scrollregion=(0,0,2000,2000),
+                       yscrollcommand=vbar.set, xscrollcommand=hbar.set, width=800, height=600)
+
+main.bind('<Button-1>', cllbck)
+main.create_image(1000,1000, image=bgpic)
                              
-right_scrlbar.pack(side=tk.RIGHT, fill=tk.Y)
-bottom_scrlbar.pack(side=tk.BOTTOM, fill=tk.X)
-main_frame.pack(side=tk.LEFT)
+vbar.grid(row=0, column=1, sticky=tk.NS)
+hbar.grid(row=1, column=0, sticky=tk.EW)
+main.grid(row=0, column=0, sticky=tk.NSEW)
 
-right_scrlbar.config(command=main_frame.yview)
-bottom_scrlbar.config(command=main_frame.xview)
+vbar.config(command=main.yview)
+hbar.config(command=main.xview)
+
+main.lbl_SOL_KOLL_T_MW = tk.Button( main, text='--.-', width=10, bg='darkgreen' )
+main.lbl_SOL_KOLL_T_MW.place( x=329, y=429 )
 
 root.mainloop()
 
