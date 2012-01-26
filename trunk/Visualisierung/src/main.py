4577349490507fdde2e3317d5bf1433fec919338
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2012-01-26 10:35:32 +0000

    GUI von Appl. trennen. Konzept steht: Call Backfunktionen / Funktionspointer

--- trunk/Visualisierung/src/main.py
+++ trunk/Visualisierung/src/main.py
@@ -4,5 +4,26 @@ Created on 23.01.2012
 @author: stegmann
 '''
 
+import threading
+
+class iTimer(threading.Thread):
+    def __init__(self, interval, target):
+        threading.Thread.__init__(self)
+        self.event = threading.Event()
+        self.interval = interval
+        self.target = target
+        
+    def run(self):
+        while not self.event.is_set():
+            """ The things I want to do go here. """
+            self.target()
+            """ Event abwarten """
+            self.event.wait(self.interval)
+
+    def stop(self):
+        self.event.set()
+
+#----
+
 if __name__ == '__main__':
     pass
--- trunk/Visualisierung/src/telnetIf.py
+++ trunk/Visualisierung/src/telnetIf.py
@@ -40,13 +40,13 @@ class TelnetInterface(telnetlib.Telnet):
             self.pu_dict[pu_name] = ''
             for line in lines:
                 if (line.startswith(pu_name)):
-                    self.pu_dict[pu_name] = str( line.split('=')[1] )
+                    self.pu_dict[pu_name] = str( line.split('=')[1] ).lstrip()
                     
         for av_name in signals.AV_NAMES:
             self.av_dict[av_name]=''
             for line in lines:
                 if (line.startswith(av_name)):
-                    self.av_dict[av_name] = str( line.split('=')[1] )
+                    self.av_dict[av_name] = str( line.split('=')[1] ).lstrip()
 
         lines = self.HoleAntwort( b"GET AO\n" )
         for mv_name in signals.MV_NAMES:
@@ -60,25 +60,51 @@ class TelnetInterface(telnetlib.Telnet):
             self.di_dict[di_name] = ''
             for line in lines:
                 if (line.startswith(di_name)):
-                    self.di_dict[di_name] = str( line.split('=')[1] )
+                    self.di_dict[di_name] = str( line.split('=')[1] ).lstrip()
         
         self.cnt_dict['WW_WZ_MW'] = 0
         return (self.t_dict, self.pu_dict, self.mv_dict, self.di_dict, self.cnt_dict, self.av_dict)
 
+    def Fb_MvReglerParamSchreiben(self, kp, ki, ap):
+        command = "put vfb 00="+str(kp)        
+        self.write( command.encode('utf-8') )
+        time.sleep(0.5)
+        command = "put vfb 01="+str(ki)
+        self.write( command.encode('utf-8') )
+        time.sleep(0.5)
+        command = "put vfb 02="+str(ap)
+        self.write( command.encode('utf-8') )
+        time.sleep(0.5) 
+    
+    def Fb_MvReglerParamLesen(self):
+        command = "get vfb -1" 
+        self.write( command.encode('utf-8') )
+        time.sleep(2.0)
+        buffer = self.read_very_eager()
+        bufdecode = buffer.decode('utf8')
+        lines = bufdecode.splitlines()
+        ap = kp = ki = 0.0
+        for line in lines:
+            if (line.startswith('(00)')):
+                kp = float(line.split('=')[1])
+            if (line.startswith('(01)')):
+                ki = float(line.split('=')[1])
+            if (line.startswith('(02)')):
+                ap = float(line.split('=')[1])
+        
+        return (kp, ki, ap)
 
 
 if __name__ == '__main__':
-    interface = TelnetInterface('192.168.2.102', 1969, 10)
-    
-    interface.ErmittleMesswerte()
-    print (interface.t_dict)
-    print (interface.av_dict)
-    print (interface.cnt_dict)
-    print (interface.di_dict)
-    print (interface.pu_dict)
-    print (interface.mv_dict)
-    print (interface.t_dict['FB_SEK_Tvl_MW'] )
-    interface.close()
+    iface = TelnetInterface('stegmann.homelinux.org', 1969, 10)   
+    iface.ErmittleMesswerte()
+    print(iface.t_dict)
+    print(iface.av_dict)
+    print(iface.cnt_dict)
+    print(iface.di_dict)
+    print(iface.pu_dict)
+    print(iface.mv_dict)
+    iface.close()
     
     
     
