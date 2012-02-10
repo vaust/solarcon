b00331b2710e9898d81a2b1d6d9d2a52fa7498a0
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2012-02-10 07:21:30 +0000

    Nicht mehr benötigtes ki_x_TA Element aus reg_par_s herausgenommen.

--- trunk/HeizungRCA_NG/inc/reg.h
+++ trunk/HeizungRCA_NG/inc/reg.h
@@ -34,7 +34,6 @@ typedef struct reg_par_s {
     float kp;               /**< Proportionalbeiwert                  */
     float ki;               /**< Integralbeiwert                      */
     float ap;               /**< Arbeitspunkt des Reglers             */
-    float ki_x_TA;          /**< Ki x TA in init() einmalig berechnen */
     float lower_limit;      /**< Untere Begrenzung des Reglerausgangs */
     float upper_limit;      /**< Obere  Begrenzung des Reglerausgangs */
 } reg_par_t;
