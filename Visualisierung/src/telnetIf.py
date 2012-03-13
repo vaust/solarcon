7bc477c28750ce1bce3136a8def4f43bb4b6cffc
Volker.Stegmann <Volker.Stegmann@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2012-03-13 08:23:25 +0000

    Vereinfachungen

--- Visualisierung/src/telnetIf.py
+++ Visualisierung/src/telnetIf.py
@@ -20,18 +20,23 @@ class TelnetInterface(telnetlib.Telnet):
         self.av = dict()
         
     def HoleAntwort(self, cmdstr):
-        self.write(cmdstr)
+        self.write( cmdstr.encode('utf-8') )
         time.sleep(0.5)
         buffer = self.read_very_eager()
         bufdecode = buffer.decode('utf8')
         lines = bufdecode.splitlines()
         return lines
-    
+
+    def SchreibeBefehl(self, cmdstr):
+        self.write( cmdstr.encode('utf-8') )
+        time.sleep(0.5)
+        self.read_very_eager()
+            
     def beenden(self):
         self.write(b"END\n")
     
     def get_T(self):
-        lines = self.HoleAntwort( b"GET T\n" )
+        lines = self.HoleAntwort( "GET T\n" )
         for name in signals.TEMP_NAMES:
             self.t[name] = 0.0
             for line in lines:
@@ -41,7 +46,7 @@ class TelnetInterface(telnetlib.Telnet):
         return(self.t)
     
     def get_DO(self):
-        lines = self.HoleAntwort( b"GET DO\n" )
+        lines = self.HoleAntwort( "GET DO\n" )
         for name in signals.PU_NAMES:
             self.pu[name] = ''
             for line in lines:
@@ -57,7 +62,7 @@ class TelnetInterface(telnetlib.Telnet):
         return(self.pu, self.av)
 
     def get_AO(self):     
-        lines = self.HoleAntwort( b"GET AO\n" )
+        lines = self.HoleAntwort( "GET AO\n" )
         for name in signals.AO_NAMES:
             self.mv[name] = 0.0
             for line in lines:
@@ -67,15 +72,19 @@ class TelnetInterface(telnetlib.Telnet):
         return(self.mv)
     
     def get_DI(self):
-        lines = self.HoleAntwort( b"GET DI\n" )
+        lines = self.HoleAntwort( "GET DI\n" )
         for name in signals.DI_NAMES:
             self.di[name] = ''
             for line in lines:
                 if (line.startswith(name)):
                     self.di[name] = str( line.split('=')[1] ).lstrip()
-        
-        self.cnt['WW_WZ_MW'] = 0
-        
+
+        for name in signals.CNT_NAMES:
+            self.cnt[name] = ''
+            for line in lines:
+                if (line.startswith(name)):
+                    self.cnt[name] = str( line.split('=')[1] ).lstrip()
+                 
         return(self.di, self.cnt)
         
     def ErmittleMesswerte(self):
@@ -86,23 +95,12 @@ class TelnetInterface(telnetlib.Telnet):
         return (self.t, self.pu, self.mv, self.di, self.cnt, self.av)
 
     def Fb_MvReglerParamSchreiben(self, kp, ki, ap):
-        command = "put vfb 00="+str(kp)+"\n"        
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5)
-        command = "put vfb 01="+str(ki)+"\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5)
-        command = "put vfb 02="+str(ap)+"\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5) 
+        self.SchreibeBefehl( "put vfb 00="+str(kp)+"\n" )        
+        self.SchreibeBefehl( "put vfb 01="+str(ki)+"\n" )
+        self.SchreibeBefehl( "put vfb 02="+str(ap)+"\n" )
     
     def Fb_MvReglerParamLesen(self):
-        command = "get vfb -1\n" 
-        self.write( command.encode('utf-8') )
-        time.sleep(3.0)
-        buffer = self.read_very_eager()
-        bufdecode = buffer.decode('utf-8')
-        lines = bufdecode.splitlines()
+        lines = self.HoleAntwort( "get vfb -1\n" )
         ap = kp = ki = 0.0
         for line in lines:
             if (line.startswith('(00)')):
@@ -120,10 +118,7 @@ class TelnetInterface(telnetlib.Telnet):
             command += '1\n'
         else:
             command += '0\n'
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5)
-        self.read_very_eager()
-        pass
+        self.SchreibeBefehl( command )
     
     def Fb_Schalte_SekPumpe(self, value):
         command = "put vfb 25="
@@ -131,22 +126,17 @@ class TelnetInterface(telnetlib.Telnet):
             command += '1\n'
         else:
             command += '0\n'
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5)
-        self.read_very_eager()
+        self.SchreibeBefehl( command )
     
     def Fb_wechsle_HandAuto(self, state):
         if (state == True):
             command = "hand fb\n"
         else:
             command = "auto fb\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(1.0)
-        self.read_very_eager()
+        self.SchreibeBefehl( command )
                 
     def Fb_leseMischventil(self):
-        command = "get vfb 19\n"
-        lines=self.HoleAntwort(command)
+        lines=self.HoleAntwort( "get vfb 19\n" )
         mv_y = -999.9
         for line in lines:
             if (line.startswith('fb.o.prim_mv_y')):
@@ -154,29 +144,15 @@ class TelnetInterface(telnetlib.Telnet):
         return mv_y
             
     def Fb_schreibeMischventil(self, value):
-        command = "put vfb 19="+str(value)+"\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(1.0)
-        self.read_very_eager()
+        self.SchreibeBefehl( "put vfb 19="+str(value)+"\n" )
         
     def WW_PuReglerParamSchreiben(self, kp, ki, ap):
-        command = "put vww 00="+str(kp)+"\n"        
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5)
-        command = "put vww 01="+str(ki)+"\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5)
-        command = "put vww 02="+str(ap)+"\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(0.5) 
-    
+        self.SchreibeBefehl( "put vww 00="+str(kp)+"\n" )   
+        self.SchreibeBefehl( "put vww 01="+str(ki)+"\n" )
+        self.SchreibeBefehl( "put vww 02="+str(ap)+"\n" )
+   
     def WW_PuReglerParamLesen(self):
-        command = "get vww -1\n" 
-        self.write( command.encode('utf-8') )
-        time.sleep(3.0)
-        buffer = self.read_very_eager()
-        bufdecode = buffer.decode('utf-8')
-        lines = bufdecode.splitlines()
+        lines = self.HoleAntwort( "get vww -1\n" )
         ap = kp = ki = 0.0
         for line in lines:
             if (line.startswith('(00)')):
@@ -189,12 +165,7 @@ class TelnetInterface(telnetlib.Telnet):
         return (kp, ki, ap)
 
     def Param_GetParam(self, values): # Parameter values ist ein dict, das CallbyRef uebergeben wird
-        command = "get param\n"
-        self.write( command.encode('utf-8') )
-        time.sleep(3.0)
-        buffer = self.read_very_eager()
-        bufdecode = buffer.decode('utf-8')
-        lines = bufdecode.splitlines()
+        lines = self.HoleAntwort( "get param\n" )
         for line in lines:
             for name in signals.PARAMETERS:
                 if (line.startswith(name)):
