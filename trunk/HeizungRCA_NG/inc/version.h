4c38b3f55849b868c0037a1dc46ae7feb08c607b
volker.stegmann@gmail.com <volker.stegmann@gmail.com@6acdf1ed-15b2-d72e-6236-d45b2babb0a1>
2012-12-03 12:00:39 +0000

    Neue Version 0.9.2

--- trunk/HeizungRCA_NG/inc/version.h
+++ trunk/HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "0.9.1\n\tBuild 2012-02-09 16:10:21\n"
+#define VERSIONSTRING "0.9.2\n\tBuild 2012-12-03 12:55:09\n"
 #define VERSIONDEVPATH "https://solarcon.googlecode.com/svn/trunk/HeizungRCA_NG\n"
 
 #endif
