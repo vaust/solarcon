6f5749cf262c70027969400d127eaa1c9a9fdbbf
volker.stegmann <volker.stegmann@gmail.com>
2013-04-04 07:50:51 +0000

    Binary auf rca9 umgenannt, damit es keinen Konflikt mit vorgaben.ini gibt.

--- HeizungRCA_NG/inc/version.h
+++ HeizungRCA_NG/inc/version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define VERSIONSTRING "1.0 beta 01\n\tBuild 2013-04-03 09:46:07\n"
+#define VERSIONSTRING "1.0 beta 02\n\tBuild 2013-04-04 09:48:40\n"
 #define VERSIONDEVPATH "https://solarcon.googlecode.com/svn/trunk/HeizungRCA_NG\n"
 
 #endif
